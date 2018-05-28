10cf8e86acc30a3d73e5a6ba8bb1d3764d4f5eff
20172104262 <376261470@qq.com>
2018-05-28 09:58:02 +0800

    Initial commit

---
+++ test1/test1.cpp
@@ -0,0 +1,52 @@
+// test1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"iostream"
+using namespace std;
+
+class Cfeet
+{
+private:
+	int inch;
+	int feet;
+public:
+	void setvalue(int a, int b);
+	void display();
+	Cfeet add(Cfeet & ojbk);
+	Cfeet operator +(Cfeet & ojbk);
+};
+void Cfeet::setvalue(int a,int b)
+{
+	feet = a + b / 12;
+	inch = b%12;
+}
+void Cfeet::display()
+{
+	cout << feet << "feet" << inch << "inch" << endl;
+}
+Cfeet Cfeet::add(Cfeet & ojbk)
+{
+	Cfeet temp;
+	temp.setvalue(feet + ojbk.feet, inch + ojbk.inch);
+	return temp;
+}
+Cfeet Cfeet::operator+(Cfeet & ojbk)
+{
+	Cfeet temp;
+	temp.setvalue(feet + ojbk.feet, inch + ojbk.inch);
+	return temp;
+}
+
+
+
+int main()
+{
+	Cfeet A, B, C;
+	A.setvalue(12, 13);
+	B.setvalue(42, 2);
+	C = A + B;
+	C.display();
+	return 0;
+}
+
